69a680f6638148871b9486469889e6c714405a04
gabycrem <gabymacre@gmail.com>
2022-07-24 20:30:03 -0300

    24-7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialesComponent } from './components/sociales/sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoAPComponent,
+    HeaderComponent,
+    SocialesComponent,
+    BannerComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
